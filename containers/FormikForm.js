859d931c417c5eef3faeed80afa0119383f5717e
lthimiri <lthimiri@gmail.com>
2020-03-11 21:13:36 -0400

    adding additional space in the FormikForm.

--- containers/FormikForm.js
+++ containers/FormikForm.js
@@ -3,7 +3,7 @@ import { Formik } from "formik";
 
 const FormikForm = () => (
   <div>
-    <h1>Anywhere in your app!</h1>
+    <h1>Anywhere in your app!!</h1>
     <Formik
       initialValues={{ email: "", password: "" }}
       validate={values => {
